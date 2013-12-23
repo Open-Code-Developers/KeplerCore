eaa437672ddeee4769cd27f8314e9f315ae97a5d
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-23 14:57:12 -0700

    Remove Empty Test Class Add ArrayHelperTest Object.

--- src/test/scala/net/keplercore/test/Test.scala
+++
@@ -1,5 +0,0 @@
-package net.keplercore.test
-
-object Test {
-
-}
---
+++ src/test/scala/net/keplercore/test/util/ArrayHelperTest.scala
@@ -0,0 +1,10 @@
+package net.keplercore.test.util
+
+import org.scalatest._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+object ArrayHelperTest extends FlatSpec with Matchers {
+
+}
