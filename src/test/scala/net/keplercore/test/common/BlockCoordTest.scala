ec755f23f4178fc0e2146e14912a999528f79fdf
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-23 16:27:37 -0700

    Testing Coordinates Array as String.

--- src/test/scala/net/keplercore/test/common/BlockCoordTest.scala
+++ src/test/scala/net/keplercore/test/common/BlockCoordTest.scala
@@ -8,8 +8,14 @@ import net.keplercore.common.BlockCoord
 @RunWith(classOf[JUnitRunner])
 class BlockCoordTest extends FlatSpec with Matchers {
 
-  "A Block Coordinate" should "be returned in String form." in {
+  "A Block Coordinate in the form of 3 integers" should "be returned in String form." in {
     val coord = new BlockCoord(0, 0, 0)
+    coord.toString shouldBe "[0 0 0]"
+  }
+
+  "A Block Coordinate in an array of integers" should "returned in String form." in {
+    val coordArray = Array(0, 0, 0)
+    val coord = new BlockCoord(coordArray)
     coord.toString shouldBe "[0 0 0]"
   }
 
