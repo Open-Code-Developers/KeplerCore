fe788a31ca0216a885d4a55126a5cea6789ed821
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-23 16:44:05 -0700

    Testing the Hash Codes.

--- src/test/scala/net/keplercore/test/common/BlockCoordTest.scala
+++ src/test/scala/net/keplercore/test/common/BlockCoordTest.scala
@@ -14,15 +14,30 @@ class BlockCoordTest extends FlatSpec with Matchers {
     coord.toString shouldBe "[0 0 0]"
   }
 
+  it should "be returned in Hash form." in {
+    val coord = new BlockCoord(0, 0, 0)
+    coord.hashCode() shouldBe 8421504
+  }
+
   "A Block Coordinate in the form of an array of integers" should "returned in String form." in {
     val coordArray = Array(0, 0, 0)
     val coord = new BlockCoord(coordArray)
     coord.toString shouldBe "[0 0 0]"
   }
 
+  it should "be returned in Hash form." in {
+    val coord = new BlockCoord(0, 0, 0)
+    coord.hashCode() shouldBe 8421504
+  }
+
   "A Block Coordinate supplied by a ForgeDirection" should "be returned in String form." in {
     val coord = new BlockCoord(ForgeDirection.getOrientation(1))
     coord.toString shouldBe "[0 1 0]"
+  }
+
+  it should "be returned in Hash form." in {
+    val coord = new BlockCoord(0, 0, 0)
+    coord.hashCode() shouldBe 8421504
   }
 
 }
