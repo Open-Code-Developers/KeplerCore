f436a21f78ea6748f7cbe9cb3e5a453fe8c6d975
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-23 15:47:32 -0700

    Testing if a block is not an air block.

--- src/test/scala/net/keplercore/test/lib/schematic/SchematicBlockTest.scala
+++ src/test/scala/net/keplercore/test/lib/schematic/SchematicBlockTest.scala
@@ -8,9 +8,14 @@ import net.keplercore.lib.schematic.SchematicBlock
 @RunWith(classOf[JUnitRunner])
 class SchematicBlockTest extends FlatSpec with Matchers {
 
-  "Air Block" should "return true if it is an air block." in {
+  "Air Block" should "return true if a block is an air block." in {
     val schematic = new SchematicBlock(0, 0, 0, 0, 0)
     schematic.isAir shouldBe true
+  }
+
+  it should "return false if a block is not an air block." in {
+    val schematic = new SchematicBlock(0, 0, 0, 1, 0)
+    schematic.isAir shouldBe false
   }
 
 }
