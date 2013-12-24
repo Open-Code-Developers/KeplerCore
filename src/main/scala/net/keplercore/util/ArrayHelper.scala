cb75d4c1d394d294d83277c8ff248ecb5b07fb8d
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-24 12:12:58 -0700

    Remove semicolons.

--- src/main/scala/net/keplercore/util/ArrayHelper.scala
+++ src/main/scala/net/keplercore/util/ArrayHelper.scala
@@ -1,22 +1,22 @@
-package net.keplercore.util;
+package net.keplercore.util
 
 public class ArrayHelper
 {
 	public static String[] joinStringArray(String[] oldArray, String... oldArray2)
 	{
-		String[] newArray = new String[oldArray.length + oldArray2.length];
-		int i;
+		String[] newArray = new String[oldArray.length + oldArray2.length]
+		int i
 
 		for (i = 0; i < oldArray.length; ++i)
 		{
-			newArray[i] = oldArray[i];
+			newArray[i] = oldArray[i]
 		}
 
 		for (i = 0; i < oldArray2.length; ++i)
-		{
-			newArray[i + oldArray.length] = oldArray2[i];
+    {
+			newArray[i + oldArray.length] = oldArray2[i]
 		}
 
-		return newArray;
+		return newArray
 	}
 }
