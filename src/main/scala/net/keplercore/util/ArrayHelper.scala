af918e0e2c41581cf74fbfdd9eeef63fdc094cc8
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-24 13:55:24 -0700

    Hey Rockey! Watch this! Not again Bullwinkle…

--- src/main/scala/net/keplercore/util/ArrayHelper.scala
+++ src/main/scala/net/keplercore/util/ArrayHelper.scala
@@ -3,8 +3,6 @@ package net.keplercore.util
 object ArrayHelper {
   def joinStringArray(oldArray: Array[String], oldArray2: String*): Array[String] =
   {
-    var newArray = new Array[String](oldArray.length + oldArray2.length)
-    newArray = oldArray ++ oldArray2
-    newArray
+    oldArray ++ oldArray2
   }
 }
