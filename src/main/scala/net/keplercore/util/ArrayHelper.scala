91e15c221f101a8acbe27bc325ce637071fc3483
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-24 12:37:51 -0700

    This should work…

--- src/main/scala/net/keplercore/util/ArrayHelper.scala
+++ src/main/scala/net/keplercore/util/ArrayHelper.scala
@@ -2,15 +2,13 @@ package net.keplercore.util
 
 class ArrayHelper
 {
-	def joinStringArray(oldArray: Array[String], oldArray2: String*): Array[String] = {
-		val newArray: Array[String] = new String[oldArray.length + oldArray2.length]
-		val i: Int = _
-		for (i = 0; i < oldArray.length; ++i) {
-			newArray[i] = oldArray[i]
-		}
-		for (i = 0; i < oldArray2.length; ++i) {
-			newArray[i + oldArray.length] = oldArray2[i]
-		}
+	def joinStringArray(oldArray: Array[String], oldArray2: String*): Array[String] =
+  {
+		val newArray: Array[String] = new Array(oldArray.length + oldArray2.length)
+		for (string: String <- oldArray2)
+    {
+      newArray.+(string)
+    }
 		newArray
 	}
 }
