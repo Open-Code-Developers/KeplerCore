80b88201df793e7e1d62d2c49d772389e9637070
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-24 12:15:59 -0700

    Rewrite some of the method name stuff and class name stuff for scala

--- src/main/scala/net/keplercore/util/ArrayHelper.scala
+++ src/main/scala/net/keplercore/util/ArrayHelper.scala
@@ -1,22 +1,16 @@
 package net.keplercore.util
 
-public class ArrayHelper
+class ArrayHelper
 {
-	public static String[] joinStringArray(String[] oldArray, String... oldArray2)
-	{
+	def joinStringArray(oldArray: Array, oldArray2: String*): Array = {
 		String[] newArray = new String[oldArray.length + oldArray2.length]
 		int i
-
-		for (i = 0; i < oldArray.length; ++i)
-		{
+		for (i = 0; i < oldArray.length; ++i) {
 			newArray[i] = oldArray[i]
 		}
-
-		for (i = 0; i < oldArray2.length; ++i)
-    {
+		for (i = 0; i < oldArray2.length; ++i) {
 			newArray[i + oldArray.length] = oldArray2[i]
 		}
-
-		return newArray
+		newArray
 	}
 }
