44d9205b8c6a7f30b8896806d89e6b98ee07d46f
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-24 13:40:57 -0700

    Compiles and Passes.

--- src/main/scala/net/keplercore/util/ArrayHelper.scala
+++ src/main/scala/net/keplercore/util/ArrayHelper.scala
@@ -3,20 +3,8 @@ package net.keplercore.util
 object ArrayHelper {
   def joinStringArray(oldArray: Array[String], oldArray2: String*): Array[String] =
   {
-    val newArray = new Array[String](oldArray.length + oldArray2.length)
-    var i: Int = 0
-    i = 0
-    while (i < oldArray.length)
-    {
-      newArray(i) = oldArray(i)
-      i
-    }
-    i = 0
-    while (i < oldArray2.length)
-    {
-      newArray(i + oldArray.length) = oldArray2(i)
-      i
-    }
+    var newArray = new Array[String](oldArray.length + oldArray2.length)
+    newArray = oldArray ++ oldArray2
     newArray
   }
 }
