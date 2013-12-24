d97911f31f64b695a8888717b60321e35f6d497d
Kepler Sticka-Jones <kbsj269@gmail.com>
2013-12-24 14:10:25 -0700

    Removed all end of line colons.

--- src/main/scala/net/keplercore/util/InventoryHelper.scala
+++ src/main/scala/net/keplercore/util/InventoryHelper.scala
@@ -1,33 +1,33 @@
-package net.keplercore.util;
+package net.keplercore.util
 
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
+import net.minecraft.entity.item.EntityItem
+import net.minecraft.inventory.IInventory
+import net.minecraft.item.ItemStack
+import net.minecraft.world.World
 
 public class InventoryHelper
 {
 	public static void dropItems(World world, ItemStack stack, int i, int j, int k) {
 		if (stack.stackSize <= 0) {
-			return;
+			return
 		}
 
-		float f1 = 0.7F;
-		double d = (world.rand.nextFloat() * f1) + (1.0F - f1) * 0.5D;
-		double d1 = (world.rand.nextFloat() * f1) + (1.0F - f1) * 0.5D;
-		double d2 = (world.rand.nextFloat() * f1) + (1.0F - f1) * 0.5D;
-		EntityItem entityitem = new EntityItem(world, i + d, j + d1, k + d2, stack);
-		entityitem.delayBeforeCanPickup = 10;
+		float f1 = 0.7F
+		double d = (world.rand.nextFloat() * f1) + (1.0F - f1) * 0.5D
+		double d1 = (world.rand.nextFloat() * f1) + (1.0F - f1) * 0.5D
+		double d2 = (world.rand.nextFloat() * f1) + (1.0F - f1) * 0.5D
+		EntityItem entityitem = new EntityItem(world, i + d, j + d1, k + d2, stack)
+		entityitem.delayBeforeCanPickup = 10
 
-		world.spawnEntityInWorld(entityitem);
+		world.spawnEntityInWorld(entityitem)
 	}
 
 	public static void dropItems(World world, IInventory inv, int i, int j, int k) {
 		for (int slot = 0; slot < inv.getSizeInventory(); ++slot) {
-			ItemStack items = inv.getStackInSlot(slot);
+			ItemStack items = inv.getStackInSlot(slot)
 
 			if (items != null && items.stackSize > 0) {
-				dropItems(world, inv.getStackInSlot(slot).copy(), i, j, k);
+				dropItems(world, inv.getStackInSlot(slot).copy(), i, j, k)
 			}
 		}
 	}
