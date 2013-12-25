20144761f1325a498cb56bab56c1d311a7cae716
Vilim <vilim.lendvaj@sk.t-com.hr>
2013-12-25 20:23:31 +0100

    WURK!!!
    You need to be louder ;P

--- src/test/scala/net/keplercore/test/lib/schematic/SchematicBlockTest.scala
+++ src/test/scala/net/keplercore/test/lib/schematic/SchematicBlockTest.scala
@@ -10,40 +10,40 @@ import net.minecraft.block.Block
 class SchematicBlockTest extends FlatSpec with Matchers
 {
 	"Air Block" should "return true if a block is an air block." in
-		{
-			val schematic: SchematicBlock = new SchematicBlock(0, 0, 0, 0, 0)
-			schematic.isAir shouldBe true
-		}
+	{
+		val schematic: SchematicBlock = SchematicBlock(0, 0, 0, 0, 0)
+		schematic.isAir shouldBe true
+	}
 
 	it should "return false if a block is not an air block." in
-		{
-			val schematic: SchematicBlock = new SchematicBlock(0, 0, 0, 1, 0)
-			schematic.isAir shouldBe false
-		}
+	{
+		val schematic: SchematicBlock = SchematicBlock(0, 0, 0, 1, 0)
+		schematic.isAir shouldBe false
+	}
 
 	"Fluid Block" should "return true if a block is a fluid block" in
-		{
-			val schematic: SchematicBlock = new SchematicBlock(0, 0, 0, Block.waterMoving.blockID, 0)
-			schematic.isAir shouldBe true
-		}
+	{
+		val schematic: SchematicBlock = SchematicBlock(0, 0, 0, Block.waterMoving.blockID, 0)
+		schematic.isAir shouldBe true
+	}
 
 	it should "return false if a block is not a fluid block" in
-		{
-			val schematic: SchematicBlock = new SchematicBlock(0, 0, 0, 1, 0)
-			schematic.isAir shouldBe false
-		}
+	{
+		val schematic: SchematicBlock = SchematicBlock(0, 0, 0, 1, 0)
+		schematic.isAir shouldBe false
+	}
 
-	"Same Blocks" should "return true if the blocks are same" in
-		{
-			val schematic1: SchematicBlock = new SchematicBlock(0, 0, 0, 1, 0)
-			val schematic2: SchematicBlock = new SchematicBlock(0, 0, 0, 1, 0)
-			schematic1.equals(schematic2) shouldBe true
-		}
+	"Same Blocks" should "return true if they are equal" in
+	{
+		val schematic1: SchematicBlock = SchematicBlock(0, 0, 0, 1, 0)
+		val schematic2: SchematicBlock = SchematicBlock(0, 0, 0, 1, 0)
+		schematic1.equals(schematic2) shouldBe true
+	}
 
-	it should "return false if the blocks are different" in
-		{
-			val schematic1: SchematicBlock = new SchematicBlock(0, 0, 0, 1, 0)
-			val schematic2: SchematicBlock = new SchematicBlock(0, 0, 0, 2, 0)
-			schematic1.equals(schematic2) shouldBe false
-		}
+	it should "return false if they are different" in
+	{
+		val schematic1: SchematicBlock = SchematicBlock(0, 0, 0, 1, 0)
+		val schematic2: SchematicBlock = SchematicBlock(0, 0, 0, 2, 0)
+		schematic1.equals(schematic2) shouldBe false
+	}
 }
